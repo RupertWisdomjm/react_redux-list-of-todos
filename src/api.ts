64f9e5414781f86ab56b54dcf29446b66af266b6
Rupert Wisdom <rupert.wisdom@yahoo.com>
2026-09-14 20:13:57 -0400

    fix: update README demo link and format code in api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,26 +1,26 @@
-import { Todo } from "./types/Todo";
-import { User } from "./types/User";
+import { Todo } from './types/Todo';
+import { User } from './types/User';
 
 // eslint-disable-next-line operator-linebreak
 const BASE_URL =
-  "https://mate-academy.github.io/react_dynamic-list-of-todos/api";
+  'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
 
 // This function creates a promise
 // that is resolved after a given delay
 function wait(delay: number): Promise<void> {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     setTimeout(resolve, delay);
   });
 }
 
 function get<T>(url: string): Promise<T> {
   // eslint-disable-next-line prefer-template
-  const fullURL = BASE_URL + url + ".json";
+  const fullURL = BASE_URL + url + '.json';
 
   // we add some delay to see how the loader works
   return wait(300)
     .then(() => fetch(fullURL))
-    .then((response) => {
+    .then(response => {
       if (!response.ok) {
         throw new Error(`Failed to fetch ${url}: ${response.status}`);
       }
@@ -29,6 +29,6 @@ function get<T>(url: string): Promise<T> {
     });
 }
 
-export const getTodos = () => get<Todo[]>("/todos");
+export const getTodos = () => get<Todo[]>('/todos');
 
 export const getUser = (userId: number) => get<User>(`/users/${userId}`);
